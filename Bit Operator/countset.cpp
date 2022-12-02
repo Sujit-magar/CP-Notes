5a615b26451a4722713376a3e165baa58570ece9
Sujit Magar <98046388+Sujit-magar@users.noreply.github.com>
2022-12-02 19:59:06 +0530

    Update countset.cpp

--- Bit Operator/countset.cpp
+++ Bit Operator/countset.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 using namespace std;
+int countsetNaive(int a){ 
+   int res=0; 
+   while(n!=0){ 
+   res++;
+   n=n&(n-1); 
+   } 
+   return res; 
+} 
 int main(){
-    cout<<"hello";
+    int n; 
+    cin>>n; 
+    cout<<countsetNaive(n);
     return 0;
 }
