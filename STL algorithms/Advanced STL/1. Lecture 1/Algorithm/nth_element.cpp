532b06bbc51b620aeaa088276cd9c642720ec577
Sujit Magar <98046388+Sujit-magar@users.noreply.github.com>
2022-12-13 11:20:22 +0530

    Update nth_element.cpp

--- STL algorithms/Advanced STL/1. Lecture 1/Algorithm/nth_element.cpp
+++ STL algorithms/Advanced STL/1. Lecture 1/Algorithm/nth_element.cpp
@@ -1,1 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define pb push_back 
 
+
+int main()
+{
+    int a[10]={1,2,3,4,5,6,3000,8,9,10}; 
+    nth_element(a,a+8,a+10);
+    for(auto i:a){
+    cout<<i<<" ";
+    }
+    return 0;
+}
