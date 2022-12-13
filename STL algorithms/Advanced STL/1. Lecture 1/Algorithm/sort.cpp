d25df05cda3c7c1bae1bd71ce36ee6389464e88a
Sujit Magar <98046388+Sujit-magar@users.noreply.github.com>
2022-12-13 11:17:23 +0530

    Update sort.cpp

--- STL algorithms/Advanced STL/1. Lecture 1/Algorithm/sort.cpp
+++ STL algorithms/Advanced STL/1. Lecture 1/Algorithm/sort.cpp
@@ -1,1 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define pb push_back 
 
+
+int main()
+{
+    int a[10]={1,2,3,4,5,6,3000,8,9,10}; 
+    sort(a, a+10);
+    for(auto i:a){
+    cout<<i<<" ";
+    }
+    return 0;
+}
